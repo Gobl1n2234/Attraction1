ac1b185f35587f7ff5379c4963d204253e67602a
Artem <footerka2@gmail.com>
2021-10-22 16:32:50 +0300

    Created skeleton project

---
+++ src/main/java/com/example/app/attractionApp/AttractionAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.app.attractionApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+public class AttractionAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttractionAppApplication.class, args);
+	}
+
+}
